1105a8ef292a7663c2222642763e58453a82726d
Pubstar <pieterhoek@msn.com>
2023-01-26 00:01:56 +0100

    Updated projects
    Made project card a component, changed styling, added another project

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,16 @@
-import movies from './images/movies.jpg'
+import ProjectCard from './components/ProjectCard';
+import moviesImg from './images/movies.jpg'
+import reactImg from './images/react.jpg'
+import githubImg from './images/github.png'
 
 function App() {
   return (
     <div className=" px-8 py-12 bg-[#2E282A] text-[#EDE7D9] font-sofia-sans h-full 2xl:px-80 xl:px-60">
       <header className=" flex justify-between items-center mb-36">
-        <span className=" cursor-pointer font-semibold">Pieter Hoek</span>
+        <div className=' flex items-center gap-2'>
+          <span className=" cursor-pointer font-semibold">Pieter Hoek</span>
+          <img className=' w-6 h-6 cursor-pointer' onClick={() => { window.open('https://github.com/Pubstar') }} src={githubImg} alt="" />
+        </div>
         <nav className=" flex gap-8">
           <span className=" cursor-pointer border-b-2 border-[#FFC914]">Home</span>
           <span className=" cursor-pointer">Contact</span>
@@ -17,13 +23,9 @@ function App() {
       </main>
       <section>
         <h2 className=" text-4xl font-bold mb-8">My Work</h2>
-        <div className=" flex">
-          <div className=" w-64 h-96 relative flex justify-center items-center rounded-3xl overflow-hidden shadow-2xl shadow-black">
-            <span className=" absolute font-bold text-4xl z-10">Netflix UI</span>
-            <div className=" w-full h-full bg-black/70 absolute over"></div>
-            <img className=" w-full h-full" src={movies} alt="project card background" />
-          </div>
-
+        <div className=" flex gap-12 flex-wrap">
+          <ProjectCard bg={moviesImg} name="Netflix UI" description="I rebuild the Netflix homepage using React and Tailwindcss, the website has no functionality as I was primarliy focused on getting the design right." githubLink="https://github.com/Pubstar/netflix" livePageLink="https://leafy-yeot-9e427e.netlify.app/" />
+          <ProjectCard bg={reactImg} name="React + Express Login" description="This app uses express, react, mongoDB and express. It's a simple register/login system. The goal here was to have a backend API that the front-end utilizes" githubLink="https://github.com/Pubstar/React-Fullstack" livePageLink="https://react-fullstack-client.onrender.com/" />
         </div>
       </section>
     </div>
---
+++ portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import githubImg from '../images/github.png'
+
+const ProjectCard = (props) => {
+    return (
+        <div className=" w-64 h-96 relative rounded-3xl overflow-hidden shadow-2xl shadow-black p-4 text-center">
+            <span className="font-bold text-3xl z-20 relative">{props.name}</span>
+            <p className=" z-20 relative mt-10">{props.description}</p>
+            <div className=" z-20 w-56 h-20 border-t-2 border-white/40 flex items-center gap-8 justify-center absolute bottom-0">
+                <img onClick={() => { window.open(props.githubLink) }} className=" w-9 h-9 cursor-pointer" src={githubImg} alt="Github Logo" />
+                <a className=" bg-[#FFC914] rounded-lg p-2 text-[#2E282A]" href={props.livePageLink}>Visit live page</a>
+            </div>
+            <div className=" w-full h-full bg-black/70 absolute z-10 top-0 left-0"></div>
+            <img className=" w-full h-full absolute object-cover top-0 left-0" src={props.bg} alt="project card background" />
+        </div>
+    )
+}
+
+export default ProjectCard
