9f5383f88b7e8b41d62dc5de0f80625fcd93e9ae
Pubstar <pieterhoek@msn.com>
2023-01-30 16:25:08 +0100

    Added contact page
    also added the steam project and some minor styling changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,34 +1,15 @@
-import ProjectCard from './components/ProjectCard';
-import moviesImg from './images/movies.jpg'
-import reactImg from './images/react.jpg'
-import githubImg from './images/github.png'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from './pages/Homepage'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <div className=" px-8 py-12 bg-[#2E282A] text-[#EDE7D9] font-sofia-sans h-full 2xl:px-80 xl:px-60">
-      <header className=" flex justify-between items-center mb-36">
-        <div className=' flex items-center gap-2'>
-          <span className=" cursor-pointer font-semibold">Pieter Hoek</span>
-          <img className=' w-6 h-6 cursor-pointer' onClick={() => { window.open('https://github.com/Pubstar') }} src={githubImg} alt="" />
-        </div>
-        <nav className=" flex gap-8">
-          <span className=" cursor-pointer border-b-2 border-[#FFC914]">Home</span>
-          <span className=" cursor-pointer">Contact</span>
-        </nav>
-      </header>
-      <main className="mb-36">
-        <h2 className=" text-[#FFC914] text-2xl">Hello, my name is Pieter,</h2>
-        <h1 className=" text-6xl mb-16 font-bold">Front-end developer.</h1>
-        <p className=" text-4xl">I'm a passionate <span className=" border-b-4 border-[#FFC914]">webdeveloper</span> with the ability to create great things, usually using <span className=" border-b-4 border-[#FFC914]">React</span></p>
-      </main>
-      <section>
-        <h2 className=" text-4xl font-bold mb-8">My Work</h2>
-        <div className=" flex gap-12 flex-wrap">
-          <ProjectCard bg={moviesImg} name="Netflix UI" description="I rebuild the Netflix homepage using React and Tailwindcss, the website has no functionality as I was primarliy focused on getting the design right." githubLink="https://github.com/Pubstar/netflix" livePageLink="https://leafy-yeot-9e427e.netlify.app/" />
-          <ProjectCard bg={reactImg} name="React + Express Login" description="This app uses express, react, mongoDB and express. It's a simple register/login system. The goal here was to have a backend API that the front-end utilizes" githubLink="https://github.com/Pubstar/React-Fullstack" livePageLink="https://react-fullstack-client.onrender.com/" />
-        </div>
-      </section>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ portfolio/src/pages/Contact.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import githubImg from '../images/github.png'
+
+const Contact = () => {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    return (
+        <div className=" min-h-screen px-8 py-12 bg-gradient-to-b from-blue-900 text-[#EDE7D9] font-sofia-sans h-full 2xl:px-80 xl:px-60 overflow-y-scroll">
+            <header className=" flex justify-between items-center mb-36">
+                <div className=' flex items-center gap-2'>
+                    <span className=" cursor-pointer font-semibold">Pieter Hoek</span>
+                    <img className=' w-6 h-6 cursor-pointer' onClick={() => { window.open('https://github.com/Pubstar') }} src={githubImg} alt="" />
+                </div>
+                <nav className=" flex gap-8">
+                    <Link to="/" className=" cursor-pointer">Home</Link>
+                    <Link to="contact" className=" cursor-pointer border-b-2 border-[#FFC914]">Contact</Link>
+                </nav>
+            </header>
+            <main>
+                <form netlify className='flex flex-col justify-center items-center'>
+                    <label htmlFor="name">Your name:</label>
+                    <input className=' mb-4 w-96 text-black' type="text" name="name" id="name" />
+                    <label htmlFor="email">Your email:</label>
+                    <input className=' mb-4 w-96 text-black' type="email" name="email" id="email" />
+                    <label htmlFor="message">Please leave a message:</label>
+                    <textarea className=' mb-4 text-black' name="message" id="message" cols="46" rows="10"></textarea>
+                    <button onClick={handleSubmit} className=' border-2 px-4 py-2 rounded-2xl' type="submit">Send</button>
+                </form>
+            </main>
+        </div>
+    )
+}
+
+export default Contact
