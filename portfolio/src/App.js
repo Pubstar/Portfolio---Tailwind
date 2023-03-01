ff6b8396029abf62fe50153183324285dc7ddfde
Pubstar <pieterhoek@msn.com>
2023-03-01 22:14:07 +0100

    Minor styling changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className=" bg-gradient-to-r from-[#14143c] via-[#2f2f75] to-[#14143c] relative text-[#EDE7D9] font-sofia-sans tracking-wider h-full 2xl:px-80 xl:px-60 sm:px-8">
-        <header className=" flex justify-between items-center mb-36 bg-white/10 pb-8 pt-4 px-4 rounded-b-3xl md:px-8">
+        <header className=" border-2 border-t-0 border-[#FFC914] flex justify-between items-center mb-36 bg-white/10 pb-8 pt-4 px-4 rounded-b-3xl md:px-8">
           <div className=' flex items-center gap-2'>
             <span className=" cursor-pointer font-semibold">Pieter Hoek</span>
             <a target="_blank" rel="noreferrer" href="https://github.com/Pubstar">
@@ -24,7 +24,7 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
-        <footer className=" h-32 w-full absolute bottom-0 left-0 flex justify-center items-center flex-col">
+        <footer className=" border-2 border-b-0 border-[#FFC914] flex justify-between items-center bg-white/10 p-8 rounded-t-3xl md:px-8">
           <p>Pieter Hoek &#169;</p>
           <a href="mailto: pieterhoek@msn.com">pieterhoek@msn.com</a>
         </footer>
