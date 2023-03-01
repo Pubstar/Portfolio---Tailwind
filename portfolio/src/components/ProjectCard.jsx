f44325567131fa69743c45b1d98db37c7faba087
Pubstar <pieterhoek@msn.com>
2023-03-01 21:26:44 +0100

    Project card animations

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import githubImg from '../images/github.png'
+import './animation.css'
 
 const ProjectCard = (props) => {
     return (
-        <div className=" w-64 h-96 relative rounded-3xl overflow-hidden shadow-xl shadow-black/40 p-4 text-center">
+        <div id='card' className=" w-64 h-96 relative rounded-3xl overflow-hidden shadow-xl shadow-black/40 p-4 text-center">
             <span className="font-bold text-3xl z-20 relative">{props.name}</span>
-            <p className=" z-20 relative mt-10">{props.description}</p>
+            <p id='text' className=" z-20 relative mt-10">{props.description}</p>
             <div className=" z-20 w-56 h-20 border-t-2 border-white/40 flex items-center gap-8 justify-center absolute bottom-0">
                 <img onClick={() => { window.open(props.githubLink) }} className=" w-9 h-9 cursor-pointer" src={githubImg} alt="Github Logo" />
                 <a className=" bg-[#FFC914] rounded-lg p-2 text-[#2E282A]" target="_blank" rel="noreferrer" href={props.livePageLink}>Visit live page</a>
